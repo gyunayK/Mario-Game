0c6f10561606449715b91bc62277fe4df66814fa
OdinOfRivia <gyunay@hotmail.dk>
2023-01-30 23:49:22 -0800

    new

--- app.js
+++ app.js
@@ -9,58 +9,94 @@ function isTouching(a, b) {
     aRect.left > bRect.left + bRect.width
   );
 }
+const body = document.querySelector("body");
+const scoreCounter = document.createElement("h1");
+let score = 0;
+body.style.backgroundColor = "black";
+body.style.color = "white";
+scoreCounter.textContent = `Score: ${score}`;
+body.appendChild(scoreCounter);
 
 const init = () => {
-  // //get the avatar
-  const getAvatar = document.querySelector(".avatar");
-  // //get the coin
-  const getCoin = document.querySelector(".coin");
+  const footStep = new Audio("./audio/smw_footstep.wav");
+  const coinSound = new Audio("./audio/smw_coin.wav");
+  moveCoin();
+  const cunt = document.querySelector("img");
+  console.dir(cunt);
 
-  moveCoin()
-  window.addEventListener("keyup", function (e) {
-        if (e.key === "ArrowDown" || e.key === "Down") {
-          moveVertical(avatar, 50);
-        }
-        if (e.key === "ArrowUp" || e.key === "Up") {
-            moveVertical(avatar, -50);
-        }
-        if (e.key === "ArrowRight" || e.key === "Left") {
-            moveHorizontal(avatar, 50);
-        }
-        if (e.key === "ArrowLeft" || e.key === "Right") {
-            moveHorizontal(avatar, -50);
-        }
-        if (isTouching(avatar, coin)) moveCoin();
-        moveMario()
-    });
+  window.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowDown" || e.key === "Down") {
+      footStep.play();
+      moveVertical(avatar, 50);
+    }
+    if (e.key === "ArrowUp" || e.key === "Up") {
+      footStep.play();
+      moveVertical(avatar, -50);
+    }
+    if (e.key === "ArrowRight" || e.key === "Right") {
+      footStep.play();
+      avatar.style.transform = "scaleX(1)";
+      moveHorizontal(avatar, 50);
+    }
+    if (e.key === "ArrowLeft" || e.key === "Left") {
+      footStep.play();
+      avatar.style.transform = "scaleX(-1)";
+      moveHorizontal(avatar, -50);
+    }
+
+    if (isTouching(avatar, coin)) {
+      moveCoin();
+      coinSound.play();
+
+      ++score;
+      scoreCounter.textContent = `Score: ${score}`;
+    }
+  });
 };
-
-// const moveMario = (currentPosition, amount, direction){
-//     if(currentPosition )
-// }
-window.addEventListener("keyup", )
 
 const moveVertical = (element, amount) => {
   const currTop = extractPos(element.style.top);
-  element.style.top = `${currTop + amount}px`;
+  const currBot = extractPos(element.style.bottom);
+  if (
+    (currTop >= innerHeight && amount > 0) ||
+    (currBot >= innerHeight && amount > 0)
+  ) {
+    return;
+  } else if ((currTop <= 0 && amount < 0) || (currBot <= 0 && amount < 0)) {
+    return;
+  } else {
+    element.style.top = `${currTop + amount}px`;
+    element.style.bottom = `${currBot + amount}px`;
+  }
 };
 
 const moveHorizontal = (element, amount) => {
   const currLeft = extractPos(element.style.left);
-  element.style.left = `${currLeft + amount}px`;
+  if (currLeft >= innerWidth && amount > 0) {
+    return;
+  } else if (currLeft <= 0 && amount < 0) {
+    return;
+  } else {
+    element.style.left = `${currLeft + amount}px`;
+  }
 };
+// const moveHorizontal = (element, amount, winWidth, offset) => {
+//   const currLeft = parseInt(element.style.left) || 100
+//   if(currLeft + amount < winWidth - offset && currLeft + amount >= 0 ){
+//       element.style.left = `${currLeft + amount}px`;
+//   }
+// };
 
 const extractPos = (position) => {
   if (!position) return 100;
+  // -1 0
   return parseInt(position.slice(0, -2));
 };
 
 const moveCoin = () => {
   const x = Math.floor(Math.random() * window.innerWidth);
   const y = Math.floor(Math.random() * window.innerHeight);
-  coin.style.top = `${x}px`;
   coin.style.top = `${y}px`;
-  coin.style.left = `${y}px`;
   coin.style.left = `${x}px`;
 };
 
